8e7e4a5960e8ce623bbcb47c95aab1af73c5ab34
Trixz <fouad.o.yt@gmail.com>
2024-02-03 20:05:12 +0200

    finished the nav bar and a bit of the speech recognition

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./styles/Home.css"
+import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition'
+
+function Home() {
+
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition,} = useSpeechRecognition()
+
+    if(!browserSupportsSpeechRecognition){
+        return <span>Browser doesn't support speech recognition</span>
+    }
+
+  return (
+    <div><p>Microphone: {listening ? "on" : "off"}</p>
+    <button onClick={SpeechRecognition.startListening}>Start</button>
+    <button onClick={SpeechRecognition.stopListening}>Stop</button>
+    <button onClick={resetTranscript}>Reset</button>
+    <p>{transcript}</p>
+    </div>
+  )
+}
+
+export default Home
