9adbbf9ed8b3b4a42804191582842325354e17ac
Trixz <fouad.o.yt@gmail.com>
2024-02-05 17:06:05 +0200

    still the same

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import "./styles/Home.css"
-import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition'
+import React from "react";
+import "./styles/Home.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function Home() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
-    const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition,} = useSpeechRecognition()
-
-    if(!browserSupportsSpeechRecognition){
-        return <span>Browser doesn't support speech recognition</span>
-    }
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition</span>;
+  }
 
   return (
-    <div><p>Microphone: {listening ? "on" : "off"}</p>
-    <button onClick={SpeechRecognition.startListening}>Start</button>
-    <button onClick={SpeechRecognition.stopListening}>Stop</button>
-    <button onClick={resetTranscript}>Reset</button>
-    <p>{transcript}</p>
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      <button onClick={listening ? SpeechRecognition.stopListening : SpeechRecognition.startListening}>Start/Stop</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
